d3a89b62091b1c7a3cc1f8625b91d365cdb18940
Sam King <kingst@gmail.com>
2018-03-30 22:10:07 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ if [ "$(git symbolic-ref --short HEAD)" != "master" ]; then
 fi
 
 if [ -z "$(git status --porcelain)" ]; then 
-    echo 'git status is clean, deploying...'
+    echo 'git status is clean, doing a push and deploying...'
 else 
     echo 'uncommitted changes, run `git status` for more information'
     exit
 fi
-
+git push
 gcloud app deploy --project=kingst-ucd --promote
 gcloud app deploy --project=kingst-ucdavis --promote
 ./cleanup_old_version.sh kingst-ucdavis
