f2f312c5be59d7d0ee7fcfd4d1600509115b7f7e
Sam King <kingst@gmail.com>
2018-03-30 22:04:59 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-#appcfg.py --email=kingst@gmail.com update .
+
+if [ -z "$(git status --porcelain)" ]; then 
+    echo 'git status is clean, deploying...'
+else 
+    echo 'uncommitted changes, run `git status` for more information'
+    exit
+fi
+
 gcloud app deploy --project=kingst-ucd --promote
 gcloud app deploy --project=kingst-ucdavis --promote
 ./cleanup_old_version.sh kingst-ucdavis
