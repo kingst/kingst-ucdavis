eee9da82155b6ff68e319cebc35941094276c355
Sam King <kingst@gmail.com>
2018-03-30 21:29:17 -0700

    dates for final paper

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #appcfg.py --email=kingst@gmail.com update .
-gcloud app deploy --project=kingst-ucd
-gcloud app deploy --project=kingst-ucdavis
+gcloud app deploy --project=kingst-ucd --promote
+gcloud app deploy --project=kingst-ucdavis --promote
+./cleanup_old_version.sh kingst-ucdavis
+./cleanup_old_version.sh kingst-ucd
