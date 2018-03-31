3dd03818325698c547870f5c5f6dcdbdacd6c65f
Sam King <kingst@gmail.com>
2018-03-30 21:58:11 -0700

    more on final paper

---
+++ cleanup_old_version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+VERSIONS=$(gcloud app versions list --project=$1 --sort-by '~version' --format 'value(version.id)')
+COUNT=0
+echo "Keeping the $2 latest versions of the $1 service"
+for VERSION in $VERSIONS
+do
+    ((COUNT++))
+    if [ $COUNT -gt "2" ]
+    then
+      echo "Going to delete version $VERSION of the $1 project."
+      gcloud app versions delete $VERSION --project=$1 -q
+    else
+      echo "Going to keep version $VERSION of the $1 project."
+    fi
+done
