9e68671b525266540471209ab89196da1bbbc0c5
Sam King <kingst@gmail.com>
2019-10-15 14:02:48 -0700

    Bump reading list

--- closeout_lecture.sh
+++ closeout_lecture.sh
@@ -9,16 +9,16 @@ slides_url=$1
 cwd=`pwd`
 
 cd ~/class/2019-fall-189e/LectureProgrammingFall2019
-#git commit -a -m "End of lecture"
+git commit -a -m "End of lecture"
 today=`date +"%Y-%m-%d"`
-#git tag ${today}
-#git push
-#git push --tags
+git tag ${today}
+git push
+git push --tags
 
 github_url="https://github.com/kingst/LectureProgrammingFall2019/releases/tag/${today}"
 
 cd ${cwd}/scripts
-#python add_lecture_to_reading_list.py "${slides_url}" "${github_url}"
+python add_lecture_to_reading_list.py "${slides_url}" "${github_url}"
 
 python download_reading_list.py ../classes/f19-ecs189e/reading_list.csv
 cd ..
