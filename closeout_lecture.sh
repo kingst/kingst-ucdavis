fe93a4fa9e10b96f84b8c6219768e63f82a22aaf
Sam King <kingst@gmail.com>
2019-10-21 08:38:36 -0700

    Bump reading list

--- closeout_lecture.sh
+++ closeout_lecture.sh
@@ -11,7 +11,7 @@ cwd=`pwd`
 cd ~/class/2019-fall-189e/LectureProgrammingFall2019
 git commit -a -m "End of lecture"
 #today=`date +"%Y-%m-%d"`
-today = '2019-10-17'
+today='2019-10-17'
 git tag ${today}
 git push
 git push --tags
