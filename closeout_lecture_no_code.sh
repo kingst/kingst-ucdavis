a7e154f50a3c4f40f0fd0c1e43def06cb199e42f
Sam King <kingst@gmail.com>
2020-01-13 19:55:57 -0800

    Update reading list link

--- closeout_lecture_no_code.sh
+++ closeout_lecture_no_code.sh
@@ -13,7 +13,7 @@ cwd=`pwd`
 cd ${cwd}/scripts
 python add_lecture_to_reading_list_no_code.py "${slides_url}"
 
-python download_reading_list.py ../classes/f19-ecs189e/reading_list.csv
+python download_reading_list.py ../classes/w20-ecs251/reading_list.csv
 cd ..
 git commit -a -m "Add slides to reading list"
 ./deploy.sh
