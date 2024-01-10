b6fb92c114834a772f69da4f78ec0f5a3a23c9de
Sam King <kingst@ucdavis.edu>
2024-01-10 13:47:01 -0800

    switch to using sha

--- closeout_lecture.sh
+++ closeout_lecture.sh
@@ -12,12 +12,11 @@ slides_url=$1
 reading_list="classes/w24-ecs189e/reading_list.csv"
 
 git commit -a -m "End of lecture"
-today=`date +"%Y-%m-%d"`
-git tag ${today}
 git push
-git push --tags
+today=`date +"%Y-%m-%d"`
+sha=`git rev-parse HEAD`
 
-github_url="https://github.com/kingst/kingst-ucdavis/releases/tag/${today}"
+github_url="https://github.com/kingst/kingst-ucdavis/tree/${sha}/inclass_programming/w24-ecs189e"
 
 python3.10 add_lecture_to_reading_list.py  "${today}" "${reading_list}" "${slides_url}" "${github_url}"
 
