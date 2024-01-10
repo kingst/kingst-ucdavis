21497a6bffa4829d5301f33fcefcad6563449791
Sam King <kingst@gmail.com>
2024-01-10 13:38:33 -0800

    End of lecture

---
+++ inclass_programming/w24-ecs189e/EmojiChat/EmojiChat/EmojiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiChatApp.swift
+//  EmojiChat
+//
+//  Created by Sam King on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainChatView()
+        }
+    }
+}
---
+++ inclass_programming/w24-ecs189e/EmojiChat/EmojiChat/MainChatView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiChat
+//
+//  Created by Sam King on 1/10/24.
+//
+
+import SwiftUI
+
+struct MainChatView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainChatView()
+}
