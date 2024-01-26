768eca7b1fe1a8f0f24d79c9a741ef89d62fc88c
Sam King <kingst@gmail.com>
2024-01-26 09:46:03 -0800

    End of lecture

--- inclass_programming/w24-ecs189e/SwiftOptionalsPlayground.playground/Contents.swift
+++ inclass_programming/w24-ecs189e/SwiftOptionalsPlayground.playground/Contents.swift
@@ -1,4 +1,4 @@
-import UIKit
+import SwiftUI
 
 var greeting = "Hello, playground"
 
@@ -8,3 +8,59 @@ func foo(bar: String?) -> Void {
     // what type is baz and what does this expression do.
     let baz = bar ?? "Bob"
 }
+
+let name: String? = String("Bob")
+
+print(name)
+if let aName = name {
+    print("My dog's name is \(aName)")
+} else {
+    print("Name is nil")
+}
+
+func dogNickName(for name: String?) -> String? {
+    guard name != nil else {
+        print("name is nil")
+        return nil
+    }
+    
+    if name == "Bob" {
+        return "Scriggins"
+    }
+    
+    return nil
+}
+
+let name1 = dogNickName(for: "Milo")
+let name2 = dogNickName(for: name)
+
+print(name2)
+
+let hasPrefix = name?.hasPrefix("D")
+
+func getOrElse(_ value: String?, defaultValue: String) -> String {
+    if let value = value {
+        return value
+    } else {
+        return defaultValue
+    }
+    
+    /*
+    guard let value = value else {
+        return defaultValue
+    }
+    
+    return value
+     */
+}
+
+let optionalName: String? = Optional.none
+
+let theName = name ?? "Milo"
+let theOtherName = optionalName ?? "Milo"
+
+print(theName)
+print(theOtherName)
+
+let dontEverDoThisInYourWork = name!
+
