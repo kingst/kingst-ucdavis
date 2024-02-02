ee8beb51f776650ca627ffee94a6a05e2884d79a
Sam King <kingst@gmail.com>
2024-02-02 11:24:44 -0800

    End of lecture

--- inclass_programming/w24-ecs189e/ClosuresPlayground.playground/Contents.swift
+++ inclass_programming/w24-ecs189e/ClosuresPlayground.playground/Contents.swift
@@ -27,3 +27,74 @@ let numbers = [1, 2, 3, 4]
 let sum = numbers.reduce(0, +)
 print(sum)
 
+let a = {
+    print("within closure before sleep")
+    sleep(5)
+    print("within closure")
+}()
+print("after closure")
+
+Task(operation: {
+    sleep(5)
+    print("within task")
+})
+sleep(10)
+print("after task")
+
+func makeIncrementor(incrementBy amount: Int) -> (() -> Int, () -> Int) {
+    var runningTotal = 0
+    func incrementor() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    func decrementor() -> Int {
+        runningTotal -= amount
+        return runningTotal
+    }
+    return (incrementor, decrementor)
+}
+let (incrementByTen, decrementByTen) = makeIncrementor(incrementBy: 10)
+incrementByTen()
+incrementByTen()
+incrementByTen()
+decrementByTen()
+
+let (incrementBySeven, _) = makeIncrementor(incrementBy: 7)
+incrementBySeven()
+incrementBySeven()
+
+incrementByTen()
+
+let referenceToIncrementByTen = incrementByTen
+referenceToIncrementByTen()
+
+func makeIncrementors() -> [() -> Int] {
+    var runningTotal = 0
+    var incrementors: [() -> Int] = []
+    for amount in 0...9 {
+        incrementors.append({
+            runningTotal += amount
+            return runningTotal
+        })
+    }
+    
+    return incrementors
+}
+
+let incrementors = makeIncrementors()
+incrementors[0]()
+incrementors[5]()
+incrementors[3]()
+
+func makeIncrementors2() -> [() -> Int] {
+    var runningTotal = 0
+    var incrementors: [() -> Int] = []
+    for amount in 0...9 {
+        runningTotal += amount
+        incrementors.append({
+            return runningTotal
+        })
+    }
+    
+    return incrementors
+}
