669870a2a529995923c633fda78caab1f29a14f5
Sam King <kingst@gmail.com>
2024-09-27 10:27:51 -0700

    starter code for class

---
+++ inclass_programming/f24-ecs150/process_syscalls/dwc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+#include <fcntl.h>
+#include <stdlib.h>
+ 
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+}
