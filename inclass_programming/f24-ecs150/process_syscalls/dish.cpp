923ab65df9a3106aa7224a36e2e4e4f01d1492d2
Sam King <kingst@gmail.com>
2024-10-02 11:50:59 -0700

    End of lecture

--- inclass_programming/f24-ecs150/process_syscalls/dish.cpp
+++ inclass_programming/f24-ecs150/process_syscalls/dish.cpp
@@ -7,6 +7,23 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  int a = 1234;
+  char *b = new char[128];
+  strcpy(b, "hello world");
 
-  
+  int ret = fork();
+  if (ret < 0) {
+    cerr << "Error calling fork" << endl;
+    return 1;
+  } else if (ret == 0) {
+    // child
+    a = 4321;
+    cout << "child a = " << a << ", b = " << b << ", my pid is " << getpid() << endl;
+  } else {
+    // parent
+    cout << "Waiting for child" << endl;
+    pid_t child = wait(NULL);
+    cout << "parent a = " << a << ", b = " << b << ", my pid is " << getpid() << ", child pid = " << child << endl;
+  }
+  return 0;
 }
