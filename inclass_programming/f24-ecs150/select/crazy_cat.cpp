24a9956011ceaa295cd6bb312f50ba5dd9a3e3a5
Sam King <kingst@gmail.com>
2024-10-09 09:39:37 -0700

    Add starter code for class today

---
+++ inclass_programming/f24-ecs150/select/crazy_cat.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/select.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+using namespace std;
+
+// we're going to ignore error checking to keep this
+// example simple, but make sure that you do full
+// error checking in any code that you write for
+// projects in this class and in general if you want
+// to actually use the software.
+
+int main(int argc, char *argv[]) {
+
+}
---
+++ inclass_programming/f24-ecs150/select/hw.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+}
