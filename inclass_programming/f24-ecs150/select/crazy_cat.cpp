617a8f4c37d99674b28b83149c6592233c97d5c8
Sam King <kingst@gmail.com>
2024-10-11 10:02:32 -0700

    End of lecture

--- inclass_programming/f24-ecs150/select/crazy_cat.cpp
+++ inclass_programming/f24-ecs150/select/crazy_cat.cpp
@@ -17,5 +17,37 @@ using namespace std;
 // to actually use the software.
 
 int main(int argc, char *argv[]) {
+  vector<int> fdVec;
+  fdVec.push_back(STDIN_FILENO);
 
+  for(int idx = 1; idx < argc; idx++) {
+    int fd = open(argv[idx], O_RDONLY);
+    fdVec.push_back(fd);
+  }
+
+  // loop through each fd one-by-one and print out the contents
+  vector<pid_t> children;
+  for (int idx = 0; idx < fdVec.size(); idx++) {
+    int pid = fork();
+    if (pid != 0) {
+      children.push_back(pid);
+      continue;
+    }
+    
+    int fd = fdVec[idx];
+    char buffer[4096];
+    int ret;
+
+    while ((ret = read(fd, buffer, sizeof(buffer))) > 0) {
+      write(STDOUT_FILENO, buffer, ret);
+    }
+
+    close(fd);
+    return 0;
+  }
+
+  for(int idx = 0; idx < children.size(); idx++) {
+    waitpid(children[idx], NULL, 0);
+  }
+  
 }
