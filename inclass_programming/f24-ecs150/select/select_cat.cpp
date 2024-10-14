2d7f267b855324d2023834aefd02c8b23ee54973
Sam King <kingst@gmail.com>
2024-10-14 08:03:22 -0700

    End of lecture

--- inclass_programming/f24-ecs150/select/select_cat.cpp
+++ inclass_programming/f24-ecs150/select/select_cat.cpp
@@ -21,9 +21,42 @@ int main(int argc, char *argv[]) {
   fdVec.push_back(STDIN_FILENO);
 
   for(int idx = 1; idx < argc; idx++) {
-    int fd = open(argv[idx], O_RDONLY);
+    int fd = open(argv[idx], O_RDONLY | O_NONBLOCK);
     fdVec.push_back(fd);
   }
 
+  while (fdVec.size() > 0) {
+    fd_set readSet;
+    int maxFd = -1;
+    FD_ZERO(&readSet);
+    
+    for (int idx = 0; idx < fdVec.size(); idx++) {
+      int fd = fdVec[idx];
+      FD_SET(fd, &readSet);
+      if (fd > maxFd) {
+	maxFd = fd;
+      }
+    }
+
+    select(maxFd + 1, &readSet, NULL, NULL, NULL);
+
+    for (int idx = 0; idx < fdVec.size(); idx++) {
+      int fd = fdVec[idx];
+      if (FD_ISSET(fd, &readSet)) {
+	// we have data, process it!
+	char buffer[4096];
+	int ret = read(fd, buffer, sizeof(buffer));
+	if (ret == 0) {
+	  // fd is empty, EOF
+	  fdVec.erase(fdVec.begin() + idx);
+	  close(fd);
+	  break;
+	} else {
+	  // we have data, process it
+	  write(STDOUT_FILENO, buffer, ret);
+	}
+      }
+    }
+  }
   
 }
