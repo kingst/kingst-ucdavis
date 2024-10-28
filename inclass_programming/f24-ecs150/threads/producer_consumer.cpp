00c4f133d90a74319abe76bb9ee126db9a362412
Sam King <kingst@gmail.com>
2024-10-28 12:16:23 -0700

    End of lecture

--- inclass_programming/f24-ecs150/threads/producer_consumer.cpp
+++ inclass_programming/f24-ecs150/threads/producer_consumer.cpp
@@ -1,4 +1,5 @@
 int numCokes = 0;
+int maxCokes = 100;
 mutex_t cokeLock;
 cond_t hasRoom;
 cond_t hasCoke;
@@ -7,19 +8,29 @@ CokeMachine machine;
 void consumer() {
   cokeLock.lock();
 
+  while (numCokes == 0) {
+    wait(hasCoke, cokeLock);
+  }
 
   machine.removeCoke();
 
+  numCokes--;
+  signal(hasRoom);
   
   cokeLock.unlock();
 }
 
 void producer() {
   cokeLock.lock();
-  
+
+  while (numCokes == maxCokes) {
+    wait(hasRoom, cokeLock);
+  }
 
   machine.addCoke();
 
+  numCokes++;
+  signal(hasCoke);
 
   cokeLock.unlock();
 }
