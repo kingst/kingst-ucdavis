a0fa3fe48d2a9201913d900fa389027d3d25407f
Sam King <kingst@gmail.com>
2024-10-30 11:54:39 -0700

    End of lecture

--- inclass_programming/f24-ecs150/threads/smokers.cpp
+++ inclass_programming/f24-ecs150/threads/smokers.cpp
@@ -1,24 +1,38 @@
 Table table;
+mutex_t tableLock;
+cond_t tableHasItems;
+cond_t tableIsEmpty;
 
 void agent() {
   while (true) {
-
+    tableLock.lock();
+    while(!table.empty()) {
+      wait(tableIsEmpty, tableLock);
+    }
     
     items = ["papers", "tobacco", "lighter"];
     table.add(items.randomPop());
     table.add(items.randomPop());
 
-    
+    broadcast(tableHasItems);
+    tableLock.unlock(); 
   }
 }
 
 void smoker(string item) {
   while (true) {
 
-
+    tableLock.lock();
+    while (table.isEmpty() || table.contains(item)) {
+      wait(tableHasItems, tableLock);
+    }
+    
     items = table.getAllItems();
     items.add(item);
+
+    signal(tableIsEmpty);
     
+    tableLock.unlock();
     smoke(items);
 
     
