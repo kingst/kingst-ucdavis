efd98c35decece5d96abd5c402d9a8518e6a7ba0
Sam King <kingst@gmail.com>
2024-11-01 13:09:29 -0700

    End of lecture

--- inclass_programming/f24-ecs150/threads/dining.cpp
+++ inclass_programming/f24-ecs150/threads/dining.cpp
@@ -3,7 +3,7 @@ bool chopstickInUse[] = [false, false, false, false, false];
 mutex_t lock;
 cond_t chopstickFree;
 
-int leftIdx(int idx) { return (idx - 1 + N) % N; }
+int leftIdx(int idx) { return idx; }
 int rightIdx(int idx) { return (idx + 1) % N; }
 
 void philosopher(int id) {
@@ -12,13 +12,27 @@ void philosopher(int id) {
     sleep(random());
 
     // is hungry, grab chopsticks
+    lock.lock();
+    // grab the left chopstick
+    while (chopstickInUse[leftIdx(id)]) {
+      wait(chopstickFree, lock);
+    }
     chopstickInUse[leftIdx(id)] = true;
-    chopstickInUse[rightIdx(id)] = true; 
 
+    // grab the right chopstick
+    while (chopstickInUse[rightIdx(id)]) {
+      wait(chopstickFree, lock);
+    }
+    chopstickInUse[rightIdx(id)] = true; 
+    lock.unlock();
+    
     //eat
     sleep(random());
 
+    lock.lock();
     chopstickInUse[leftIdx(id)] = false;
     chopstickInUse[rightIdx(id)] = false;
+    broadcast(chopstickFree);
+    lock.unlock();
   }
 }
--- inclass_programming/f24-ecs150/threads/rwlock.cpp
+++ inclass_programming/f24-ecs150/threads/rwlock.cpp
@@ -1,16 +1,37 @@
 int numReaders = 0;
+int writersWaiting = 0;
 bool writerActive = false;
 mutex_t lock;
 cond_t unlockCalled;
 
 void reader() {
+  lock.lock();
+  while (writerActive || writersWaiting > 0) {
+    wait(unlockCalled, lock);
+  }
 
+  numReaders++;
+  unlock();
 }
 
 void writer() {
-
+  lock.lock();
+  writersWaiting++;
+  while (numReaders > 0 || writerActive) {
+    wait(unlockCalled, lock);
+  }
+  writersWaiting--;
+  writerActive = true;
+  lock.unlock();
 }
 
 void unlock() {
-
+  lock.lock();
+  if (numReaders > 0) {
+    numReaders--;
+  } else {
+    writerActive = false;
+  }
+  broadcast(unlockCalled);
+  lock.unlock();
 }
