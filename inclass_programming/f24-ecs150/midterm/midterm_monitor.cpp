0948d8a024bd0a9cd82a2958ec49075f918e28f3
Sam King <kingst@gmail.com>
2024-11-08 10:18:21 -0800

    add stubs for midterm solutions

---
+++ inclass_programming/f24-ecs150/midterm/midterm_monitor.cpp
@@ -0,0 +1,20 @@
+// solve using a thread pool
+int main(int argc, char *argv[]) {
+  jobs = parseMakefile(argc, argv);
+
+}
+
+void jobThread(void *threadArg) {
+
+}
+
+
+// solve using a thread per job
+int main(int argc, char *argv[]) {
+  jobs = parseMakefile(argc, argv);
+
+}
+
+void jobThread(void *threadArg) {
+
+}
---
+++ inclass_programming/f24-ecs150/midterm/midterm_pipe.cpp
@@ -0,0 +1,5 @@
+int main(int argc, char *argv[]) {
+  string lsDirectoryPath = string(argv[1]);
+  string grepKeyword = string(argv[2]);
+  
+}
