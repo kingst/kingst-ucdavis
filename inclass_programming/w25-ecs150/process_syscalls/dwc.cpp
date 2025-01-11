8dd73aa164a5483e55858815203ff53691e501bc
Sam King <kingst@gmail.com>
2025-01-11 06:39:19 -0800

    dwc example from class

--- inclass_programming/w25-ecs150/process_syscalls/dwc.cpp
+++ inclass_programming/w25-ecs150/process_syscalls/dwc.cpp
@@ -18,6 +18,35 @@ int main(int argc, char *argv[]) {
     fd = STDIN_FILENO;
   } else {
     fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+      cerr << "Error opening " << argv[1] << endl;
+      return 1;
+    }
+  }
+
+  int bytesRead = 0;
+  char buffer[4096];
+  int ret;
+  // what is going on with this while loop and buffer???
+  while ((ret = read(fd, buffer, sizeof(buffer))) > 0) {
+    bytesRead += ret;
+  }
+
+  if (ret == -1) {
+    cerr << "Error reading file descriptor" << endl;
+    return 1;
+  }
+
+  // replace this cout call with logic that uses write
+  stringstream stringStream;
+  stringStream << "Bytes read: " << bytesRead << endl;
+  string str = stringStream.str();
+  ret = write(STDOUT_FILENO, str.c_str(), str.size());
+
+  if (ret == -1) {
+    cerr << "Could not write to STDOUT" << endl;
+    return 1;
   }
   
+  return 0;
 }
