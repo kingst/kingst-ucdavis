d28b09322b04eca05069909de1ebc2ecde71b860
Sam King <kingst@gmail.com>
2025-01-13 10:02:03 -0800

    Bump reading list

---
+++ inclass_programming/w25-ecs150/process_syscalls/dish.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int a = 1234;
+  char *b = new char[128];
+  strcpy(b, "hello world");
+
+  cout << "child a = " << a << ", b = " << b << ", my pid is " << getpid() << endl;
+
+  return 0;
+}
