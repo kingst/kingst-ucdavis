6cf4d44478cd5ff8cec19e7e977864c60bede059
Sam King <kingst@gmail.com>
2025-01-15 12:14:38 -0800

    End of lecture

--- inclass_programming/w25-ecs150/process_syscalls/dish.cpp
+++ inclass_programming/w25-ecs150/process_syscalls/dish.cpp
@@ -11,10 +11,23 @@ int main(int argc, char *argv[]) {
   char *b = new char[128];
   strcpy(b, "hello world");
 
-  cout << "before fork pid " << getpid() << endl;
-  fork();
-  fork();
-  cout << "child a = " << a << ", b = " << b << ", my pid is " << getpid() << endl;
+  // We're ignoring error checking!
+  if (fork() == 0) {
+    cout << "child a = " << a << ", b = " << b << ", my pid is " << getpid() << endl;
+    char *args[3];
+    args[0] = strdup("ls");
+    args[1] = strdup("-a");
+    args[2] = NULL;
+    execvp(args[0], args);
+    cout << "Ok, we're done with exec" << endl;
+  } else {
+    a = 4321;
+    cout << "Parent waiting" << endl;
+    wait(NULL);
+    cout << "parent a = " << a << ", b = " << b << ", my pid is " << getpid() << endl;
+  }
 
+  cout << "all done" << endl;
+  
   return 0;
 }
