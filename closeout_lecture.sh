9f52923bf5e979a36512d77460c1d017f2cf1699
Sam King <kingst@gmail.com>
2025-01-17 06:15:07 -0800

    End of lecture

--- closeout_lecture.sh
+++ closeout_lecture.sh
@@ -14,9 +14,9 @@ reading_list="classes/w25-ecs150/reading_list.csv"
 git commit -a -m "End of lecture"
 git push
 #today=`date +"%Y-%m-%d"`
-today="2025-01-10"
-sha=`git rev-parse HEAD`
-
+today="2025-01-13"
+#sha=`git rev-parse HEAD`
+sha="409f0506a7947d313fc5807730465a11fca4b18d"
 github_url="https://github.com/kingst/kingst-ucdavis/tree/${sha}/inclass_programming/w25-ecs150"
 
 python3.10 add_lecture_to_reading_list.py  "${today}" "${reading_list}" "${slides_url}" "${github_url}"
