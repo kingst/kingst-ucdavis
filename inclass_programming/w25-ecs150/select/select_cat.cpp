f62d894b358d455e4396209fff24a413409d18f4
Sam King <kingst@gmail.com>
2025-01-24 12:13:12 -0800

    End of lecture

---
+++ inclass_programming/w25-ecs150/select/select_cat.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/select.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+using namespace std;
+
+// we're going to ignore error checking to keep this
+// example simple, but make sure that you do full
+// error checking in any code that you write for
+// projects in this class and in general if you want
+// to actually use the software.
+
+int main(int argc, char *argv[]) {
+  vector<int> fdVec;
+  fdVec.push_back(STDIN_FILENO);
+
+  for(int idx = 1; idx < argc; idx++) {
+    int fd = open(argv[idx], O_RDONLY | O_NONBLOCK);
+    fdVec.push_back(fd);
+  }
+
+  while (fdVec.size() > 0) {
+    
+    fd_set readSet;
+    int maxFd = -1;
+    FD_ZERO(&readSet);
+    // loop through each fd one-by-one and print out the contents
+    for (int idx = 0; idx < fdVec.size(); idx++) {
+      int fd = fdVec[idx];
+      FD_SET(fd, &readSet);
+      if (fd > maxFd) {
+	maxFd = fd;
+      }
+    }
+
+    select(maxFd + 1, &readSet, NULL, NULL, NULL);
+
+    for (int idx = 0; idx < fdVec.size(); idx++) {
+      int fd = fdVec[idx];
+      if (FD_ISSET(fd, &readSet)) {
+	char buffer[4096];
+	int ret = read(fd, buffer, sizeof(buffer));
+
+	if (ret == 0) {
+	  fdVec.erase(fdVec.begin() + idx);
+	  close(fd);
+	  break;
+	} else {
+	  write(STDOUT_FILENO, buffer, ret);
+	}
+      }
+    }
+  }
+    
+  cout << "all done!" << endl;
+  
+  return 0;
+}
