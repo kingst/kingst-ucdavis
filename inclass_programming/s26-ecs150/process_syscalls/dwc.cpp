df0e9545fe452529051757e36e202e0851189685
Yang Zhou <yangzhou.rpc@gmail.com>
2026-04-01 15:43:14 -0700

    adding dwc after lecture 2

---
+++ inclass_programming/s26-ecs150/process_syscalls/dwc.cpp
@@ -0,0 +1,32 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int fd;
+    if (argc == 1) {
+        fd = STDIN_FILENO;
+    } else {
+        fd = open(argv[1], O_RDONLY);
+        if (fd < 0) {
+            cerr << "could not open " << argv[1] << endl;
+            return 1;
+        }
+    }
+
+    char buffer[4096];
+    int byteRead = 0;
+    int ret;
+    while ((ret = read(fd, buffer, 4096)) > 0) {
+        byteRead += ret;
+    }
+
+    cout << "Byte count: " << byteRead << endl;
+}
