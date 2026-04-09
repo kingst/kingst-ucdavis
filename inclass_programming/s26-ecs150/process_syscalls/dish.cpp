8dc3bdf4b77f6f8ff0f6857439ee7215385fbd19
Yang Zhou <yangzhou.rpc@gmail.com>
2026-04-08 19:56:41 -0700

    add dish

--- inclass_programming/s26-ecs150/process_syscalls/dish.cpp
+++ inclass_programming/s26-ecs150/process_syscalls/dish.cpp
@@ -18,9 +18,17 @@ int main(int argc, char* argv[]) {
         return 0;
     } else if (ret == 0) {
         // this is the child;
+        char* args[3];
+        args[0] = strdup("ls");
+        args[1] = strdup("-a");
+        args[2] = NULL;
+        execvp(args[0], args);
         cout << "Child  a = " << a << ", b = " << b << ", my pid = " << getpid() << endl;
     } else {
-        cout << "Parent a = " << a << ", b = " << b << ", my pid = " << getpid() << endl;
+        // this is the parent;
+        cout << "Waiting for child" << endl;
+        pid_t child = wait(NULL);
+        cout << "Parent a = " << a << ", b = " << b << ", my pid = " << getpid() << ", child pid " << child << endl;
     }
 
     return 0;
